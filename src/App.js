b8ed1d89167188e1d18c3b4dd4983bc4211dc6d6
sekar0902 <114892530+sekar0902@users.noreply.github.com>
2024-04-06 16:28:56 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Pages/Home';
+import Reporting from './components/Pages/Reporting';
+import Interactivemap from './components/Pages/Interactivemap';
+import Predictive from './components/Pages/Predictive';
+import Adfeatue from './components/Pages/Adfeatue';
+import Dashboard from './components/Pages/Dashboard';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/map" element={<Interactivemap />} />
+          <Route path="/reporting" element={<Reporting />} />
+          <Route path="/predictive" element={<Predictive />} />
+          <Route path="/adfeature" element={<Adfeatue />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
